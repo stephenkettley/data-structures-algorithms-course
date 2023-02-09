75b1fe09918ce75cccfe502e0d24aeb1bdcb3fd9
Stephen Kettley <kettleystephen@gmail.com>
2023-02-09 12:32:51 +0200

    implemented linear search algorithm

---
+++ src/linear_search.py
@@ -0,0 +1,36 @@
+def linear_search(list: list, target: int) -> int | None:
+    """Linear search of a target number in a list of numbers.
+
+    Args:
+        list (list): list of numbers to be searched.
+        target (int): number being searched for.
+
+    Returns:
+        int: position of target number, returns None if not found.
+
+    """
+
+    for i in range(len(list)):
+        if list[i] == target:
+            return i
+
+    return None
+
+
+def verify(index: int):
+    """Verifies if number was found in a list or not.
+
+    Args:
+        index (int): index of target number.
+
+    """
+    if index is not None:
+        print(f"Target found at index {index}")
+    else:
+        print("Target not found.")
+
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+result = linear_search(nums, 6)
+verify(result)
